8cae92f3b84fa9efdecfd0032bc45209bd6532a7
Moises Lopez <moylop260@vauxoo.com>
2015-09-30 02:59:11 -0400

    [REF] runbot90: Small fixes

--- runbot90/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ${HOME}/.ssh/id_rsa "$(dirname $0)/files/id_rsa"
-# docker build -t runbot:90 $1 "$(dirname $0)"
-ansible/start.sh
--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
 set -e
 
+# Change owner all volumes to avoid conflicts with uid
 chown -R runbot:runbot /home/runbot
+chown root:root -R /var/lib/docker
+chown root:root -R /var/log/supervisor
 chmod ugo+rwx /tmp
 
+# Set psql environment variables
 source /home/runbot/.db_source
+
+# Start supervisor with all daemon
 /usr/bin/supervisord
+
