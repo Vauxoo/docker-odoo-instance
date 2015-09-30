3efff33f5547df4523e72b6de7577c3311ae2876
Moises Lopez <moylop260@vauxoo.com>
2015-09-30 03:06:38 -0400

    squash

--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -7,6 +7,11 @@ chown root:root -R /var/lib/docker
 chown root:root -R /var/log/supervisor
 chmod ugo+rwx /tmp
 
+# Re-add ssh keyscan, support overwrite of -v ssh
+ssh-keyscan github.com > /home/runbot/.ssh/known_hosts \
+&& ssh-keyscan launchpad.net >> /home/runbot/.ssh/known_hosts \
+&& ssh-keyscan bitbucket.org >> /home/runbot/.ssh/known_hosts
+
 # Set psql environment variables
 source /home/runbot/.db_source
 
