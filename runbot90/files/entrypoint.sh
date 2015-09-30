2c066b9297a8a3d4655a5a0254c88f950a3745cf
Moises Lopez <moylop260@vauxoo.com>
2015-09-30 01:07:12 -0400

    Change id_rsa from copy to volume

--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# chown -R runbot:runbot ${EXTRA_ADDONS}/odoo-extra/runbot/static
-# chown -R runbot:runbot /home/runbot/.local/share/Odoo/filestore
-# chown -R runbot:runbot /var/log/supervisor
-# chown -R runbot:runbot /var/lib/docker
 chown -R runbot:runbot /home/runbot
 chmod ugo+rwx /tmp
 
